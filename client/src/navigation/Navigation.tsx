1ffb6bb49d1087ea509db6e569aa4f0ef451dcd8
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-01-03 17:14:41 +0530

    implement login component and signup component

--- client/src/navigation/Navigation.tsx
+++ client/src/navigation/Navigation.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Alert from "../components/alert/Alert";
+import { Routes, Route } from "react-router-dom";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
 
 const Navigation: React.FC = () => {
 
     return (
         <>
             <Alert />
+            <Routes>
+                <Route path="/auth/signup" element={<Signup/>} />
+                <Route path="/auth/login" element={<Login/>} />
+            </Routes>
         </>
     );
 }
