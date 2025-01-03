09916ed2014fbfde296403da79f7595fb71d2b01
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-01-03 10:09:51 +0530

    implement auth context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navigation from "./navigation/Navigation";
 import AlertProviders from "./contexts/alert/AlertProviders";
+import AuthProviders from "./contexts/auth/AuthProviders";
 
 const App: React.FC = () => {
 
   return (
     <>
       <AlertProviders>
+        <AuthProviders>
         <Navigation />
+        </AuthProviders>
       </AlertProviders>
     </>
   );
