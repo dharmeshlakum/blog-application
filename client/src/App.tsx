185a6962b8ea1930a7c1dfdf851a3b6d6830ff77
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-01-03 09:59:11 +0530

    implement alert context in app.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navigation from "./navigation/Navigation";
+import AlertProviders from "./contexts/alert/AlertProviders";
 
 const App: React.FC = () => {
 
   return (
     <>
-      <Navigation />
+      <AlertProviders>
+        <Navigation />
+      </AlertProviders>
     </>
   );
 }
